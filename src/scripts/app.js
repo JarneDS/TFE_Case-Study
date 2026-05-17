47938a62cefbb7a6ea2c4966396c4a07acff92c5
Jarne <descheerderjarne@gmail.com>
2026-05-17 16:08:17 +0200

    ajout des observations d'intersection

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,3 +8,22 @@ function toggleMenu() {
     var menu = document.querySelector(".menu");
     menu.classList.toggle("menu--open");
 };
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".sideNav a");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        const id = entry.target.getAttribute("id");
+        const link = document.querySelector(`.sideNav a[href="#${id}"]`);
+
+        if (entry.isIntersecting) {
+            navLinks.forEach(a => a.classList.remove("active"));
+            link.classList.add("active");
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+sections.forEach(section => observer.observe(section));
