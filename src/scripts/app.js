aaaebf7cfa28d91bfb9681aa23c6947120377cd2
Jarne <descheerderjarne@gmail.com>
2026-05-28 10:54:22 +0200

    correction orthographe + correction anims GSAP

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,32 +7,145 @@ gsap.registerPlugin(ScrollTrigger);
 
 /* GSAP */
 function initGSAPAnimations() {
-    document.querySelectorAll(".col__img--left, .col--left").forEach(el => {
-        gsap.from(el, {
-            scrollTrigger: {
-                trigger: el,
-                start: "top 90%",
-            },
-            xPercent: -100,
-            opacity: 0,
-            duration: 0.8,
-            ease: "power3.out",
-            clearProps: "transform"
-        });
+    ScrollTrigger.matchMedia({
+
+        // Desktop uniquement (≥ 900px)
+        "(min-width: 900px)": function() {
+
+            document.querySelectorAll(".col__img--left, .col--left").forEach(el => {
+                gsap.from(el, {
+                    scrollTrigger: {
+                        trigger: el,
+                        start: "top 90%",
+                    },
+                    xPercent: -100,
+                    opacity: 0,
+                    duration: 0.8,
+                    ease: "power3.out",
+                    clearProps: "transform"
+                });
+            });
+
+            document.querySelectorAll(".col__img--right, .col--right").forEach(el => {
+                gsap.from(el, {
+                    scrollTrigger: {
+                        trigger: el,
+                        start: "top 90%",
+                    },
+                    xPercent: 100,
+                    opacity: 0,
+                    duration: 0.8,
+                    ease: "power3.out",
+                    clearProps: "transform"
+                });
+            });
+
+            document.querySelectorAll(".ajoutApp__card").forEach(el => {
+
+                let config = {
+                    scrollTrigger: {
+                        trigger: el,
+                        start: "top 95%",
+                    },
+                    opacity: 0,
+                    duration: 0.8,
+                    ease: "power3.out",
+                    clearProps: "transform"
+                };
+
+                if (el.classList.contains("ajoutApp__cardleft")) {
+                    config.xPercent = -100;
+                }
+
+                if (el.classList.contains("ajoutApp__cardright")) {
+                    config.xPercent = 100;
+                }
+
+                if (el.classList.contains("ajoutApp__cardmiddel")) {
+                    config.yPercent = -100;
+                    config.scrollTrigger.start = "top 20%";
+                }
+
+                if (el.classList.contains("ajoutApp__cardmiddel2")) {
+                    config.yPercent = 100;
+                    config.scrollTrigger.start = "top 120%";
+                }
+
+                gsap.from(el, config);
+            });
+        },
+
+        // Mobile (< 900px)
+        "(max-width: 899px)": function() {
+
+            document.querySelectorAll(".col__img--left, .col--left").forEach(el => {
+                gsap.from(el, {
+                    scrollTrigger: {
+                        trigger: el,
+                        start: "top 100%",
+                    },
+                    xPercent: -50,
+                    opacity: 0,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            });
+
+            document.querySelectorAll(".col__img--right, .col--right").forEach(el => {
+                gsap.from(el, {
+                    scrollTrigger: {
+                        trigger: el,
+                        start: "top 100%",
+                    },
+                    xPercent: 50,
+                    opacity: 0,
+                    duration: 0.6,
+                    ease: "power3.out",
+                });
+            });
+
+            document.querySelectorAll(".ajoutApp__card").forEach(el => {
+
+                let config = {
+                    scrollTrigger: {
+                        trigger: el,
+                        start: "top 100%",
+                    },
+                    opacity: 0,
+                    duration: 0.8,
+                    ease: "power3.out",
+                };
+
+                if (el.classList.contains("ajoutApp__cardleft")) {
+                    config.xPercent = -50;
+                }
+
+                if (el.classList.contains("ajoutApp__cardright")) {
+                    config.xPercent = 50;
+                }
+
+                if (el.classList.contains("ajoutApp__cardmiddel")) {
+                    config.yPercent = -50;
+                }
+
+                if (el.classList.contains("ajoutApp__cardmiddel2")) {
+                    config.yPercent = 50;
+                }
+
+                gsap.from(el, config);
+            });
+        }
     });
 
-    document.querySelectorAll(".col__img--right, .col--right").forEach(el => {
-        gsap.from(el, {
-            scrollTrigger: {
-                trigger: el,
-                start: "top 90%",
-            },
-            xPercent: 100,
-            opacity: 0,
-            duration: 0.8,
-            ease: "power3.out",
-            clearProps: "transform"
-        });
+
+    let mockups = document.querySelector(".mockups");
+
+    gsap.from(mockups, {
+        yPercent: 100,
+        opacity: 0,
+        duration: 0.8,
+        ease: "power3.out",
+        clearProps: "transform"
     });
 }
 
