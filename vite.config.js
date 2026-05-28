b011b0f8ccad062fe329c1a042e8e34e9ff8feeb
Jarne <descheerderjarne@gmail.com>
2026-05-28 11:44:14 +0200

    correction liens web

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
-  base: '/projets/TFE__Site/',
+  base: '/projets/tfe-site/',
   publicDir: '../public',
   build: {
     outDir: '../dist',
